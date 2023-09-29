86b86ff7161bf2d850466c115aed0ef44486faec
xelphin <111276630+xelphin@users.noreply.github.com>
2023-09-29 17:23:44 +0300

    Implement various colouring styles

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 // CONSTANTS
 const darkColor = "#011053";
+const rainbowColors = ["#ffadad","#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff", "#a0c4ff", "#bdb2ff", "#ffc6ff" ];
 
 // GLOBALS
 let boardSize = 16;
+let style = "block-colored-blue";
+let rainbowIndex = 0;
 
 // COLLECT DOM ELEMENTS
 let gridDiv = document.querySelector('#grid-div');
 let slider = document.querySelector('.slider');
 let sliderValue = document.querySelector('.slider-value');
 let resetButton = document.querySelector('#reset-btn');
+// Styles
+const styles = document.querySelectorAll('.style-btn');
+let stylesArr = Array.from(styles);
 
 // --------------------------------------------
 //              HELPER FUNCTIONS
@@ -19,18 +25,31 @@ function getPercent(amount) {
     return aPortion.toFixed(4).toString(10);
 }
 
+function getRainbowColor() {
+    let color = rainbowColors[rainbowIndex];
+    rainbowIndex = (rainbowIndex + 1 ) % rainbowColors.length;
+    return color;
+}
+
 // --------------------------------------------
 //                  FUNCTIONS
 // --------------------------------------------
 function changeColor (event) {
-    event.target.className = 'grid-block block-colored-fully';
+    event.target.style.removeProperty('background-color');
+    event.target.className = "grid-block";
+    event.target.classList.add(style);
+
+    // Case Rainbow
+    if (style == "block-colored-rainbow") {
+        event.target.style.backgroundColor = getRainbowColor();
+    }
 }
 
 function populateGrid (grid) {
     for (let i=0; i < (boardSize*boardSize); i++) {
         // Create grid block
         const gridBlock = document.createElement('div');
-        gridBlock.classList.add('grid-block');
+        gridBlock.className = 'grid-block';
         // Style grid block
         gridBlock.style.width = getPercent(boardSize) + "%";
         gridBlock.style.height = getPercent(boardSize) + "%";
@@ -48,7 +67,7 @@ function resetGridEntirely() {
     while (gridDiv.firstChild) {
         gridDiv.removeChild(gridDiv.firstChild);
     }
-    //repopulate grid
+    // Repopulate grid
     populateGrid(gridDiv);
 }
 
@@ -56,7 +75,30 @@ function resetGridColor() {
     console.log("Resetting Grid Color");
     for (let child of gridDiv.children) {
         child.className = 'grid-block block-empty';
+        child.style.removeProperty('background-color');
     }
+    
+}
+
+function changeStyle(event) {
+    let pickedStyle = event.target.getAttribute('data-style');
+    switch (pickedStyle) {
+        case "blue":
+            console.log("style: dark blue");
+            style = "block-colored-blue";
+            break;
+        case "gold":
+            console.log("style: gold");
+            style = "block-colored-gold";
+            break;
+        case "rainbow":
+            console.log("style: rainbow");
+            style = "block-colored-rainbow";
+            break;
+        default:
+            console.log("style: something else");
+    }
+    resetGridColor();
 }
 
 
@@ -78,3 +120,7 @@ slider.oninput = function() {
     // Resey grid
     resetGridEntirely()
 }
+
+stylesArr.forEach( (styleBtn) => {
+    styleBtn.addEventListener("click", (event) => changeStyle(event) );
+});
